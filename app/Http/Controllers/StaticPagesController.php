c735a009f3a555ed875f18d991040ae4428b8f6a
ztt <835303992@qq.com>
2019-12-19 18:55:29 +0800

    配置文件

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -8,7 +8,16 @@ class StaticPagesController extends Controller
 {
     public function home()
     {
-       return view('staticPages/home');
+        $option = 'A';
+       for($i=1;$i<=4;$i++) {
+            echo $option++."<br>";
+            echo sprintf('%03d',$i);
+       }
+
+       $data = explode(',','A,B,C');
+       dump($data);
+       //mysql 数据结构优化
+       //整型可以存储int数据类型，
     }
 
     public function help()
@@ -18,6 +27,20 @@ class StaticPagesController extends Controller
 
     public function about()
     {
+        // $this->display();
         return view('staticPages/about');
+    }
+
+    public function about2()
+    {
+        return $this->error($this->display());
+    }
+
+    public function about3()
+    {
+        $sql = "select * from user a left join user_log b on a.id = b.uid group by add_time order by id DESC limit 10";
+        $this->db_retail = $db;
+        $result = $db->query($sql);
+        dump($result);
     }
 }
